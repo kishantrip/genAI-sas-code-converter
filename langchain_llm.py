9407a751d1340b6cdec7406892a1e300acb8c011
Kishan Pragya Laptop <kt90090@gmail.com>
2023-11-25 22:36:17 +0530

    llm fix

--- langchain_llm.py
+++ langchain_llm.py
@@ -1,11 +1,18 @@
+import openai
 from langchain.chat_models import ChatOpenAI
+# from langchain.llms import OpenAI
 from langchain.prompts.chat import ChatPromptTemplate
 from langchain.schema.output_parser import StrOutputParser
 import time
 import os
-from io import StringIO
+# from io import StringIO
 import re
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.cache import InMemoryCache
+from langchain.globals import set_llm_cache
+
+set_llm_cache(InMemoryCache())
+
 
 # chat_model = ChatOpenAI()
 
@@ -23,20 +30,23 @@ def split_text(text, max_chunk_size=2000):
         chunks.append(current_chunk.strip())
     return chunks
 
+
 def remove_comments(string):
     pattern = r"(\".*?(?<!\\)\"|\'.*?(?<!\\)\')|(/\*.*?\*/|//[^\r\n]*$)"
     # first group captures quoted strings (double or single)
     # second group captures comments (//single-line or /* multi-line */)
-    regex = re.compile(pattern, re.MULTILINE|re.DOTALL)
+    regex = re.compile(pattern, re.MULTILINE | re.DOTALL)
 
     def _replacer(match):
         # if the 2nd group (capturing comments) is not None,
         # it means we have captured a non-quoted (real) comment string.
         if match.group(2) is not None:
             return ""  # so we will return empty to remove the comment
-        else: # otherwise, we will return the 1st group
-            return match.group(1) # captured quoted-string
+        else:  # otherwise, we will return the 1st group
+            return match.group(1)  # captured quoted-string
+
     return regex.sub(_replacer, string)
+
 
 def txt_processing(upload_file):
     # string_data = StringIO(upload_file.getvalue().decode("utf-8")).read()
@@ -53,11 +63,14 @@ def txt_processing(upload_file):
     # return string_data
     return without_empty_lines
 
+
+def _context_length_check(chunks: list, max_length: int):
+    return [i for i, item in enumerate(chunks) if len(item) > max_length]
+
+
 code = open("Mobikwik_Exe_Jul23.sas", "r").read()
 
 processed_code = txt_processing(code)
-
-
 
 # input_code_language = "sas"
 # output_code_language = "pyspark"
@@ -65,16 +78,14 @@ processed_code = txt_processing(code)
 # chunk_num = 1
 
 
-
 # def prompt_generator(code_chunk, chunk_num, total_chunks, input_code_language, output_code_language):
 
-template = f"""You are expert in converting sas code to pyspark code. All condition should be coded nothing to be skipped. 
+template = """You are expert in converting sas code to pyspark code. All condition should be coded nothing to be skipped
 Code is long so will be given in multiple parts which will be delimited with triple backticks.
 Create spark session only in part1 of the code and skip for rest all parts.
 """
 
 human_template = "{code}"
-
 
 chat_prompt = ChatPromptTemplate.from_messages([
     ("system", template),
@@ -87,29 +98,35 @@ chat_prompt = ChatPromptTemplate.from_messages([
 #                             chunk_num=chunk_num
 #                             )
 
-model = ChatOpenAI(model="gpt-3.5-turbo-16k", temperature=0)
+model = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.0, request_timeout=180,
+                   max_retries=0, cache=True)
+model2 = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.0, request_timeout=300,
+                    max_retries=0, cache=True)
+
+# model = OpenAI(model="babbage", temperature=0.0, request_timeout=180, max_retries=0)
+max_chunk = 2500
+
 chain = chat_prompt | model | StrOutputParser()
 # chain = chat_prompt | model
 
 # code_chunk = split_text(processed_code, max_chunk_size=2000)
 regex_pat = re.compile(r'RUN;', flags=re.IGNORECASE)
-processed_code = re.sub(regex_pat,'**splitpoint**', processed_code)
+processed_code = re.sub(regex_pat, '**splitpoint**', processed_code)
 
 regex_pat = re.compile(r'QUIT;', flags=re.IGNORECASE)
-processed_code = re.sub(regex_pat,'**splitpoint**', processed_code)
+processed_code = re.sub(regex_pat, '**splitpoint**', processed_code)
 
 bb = CharacterTextSplitter(separator='**splitpoint**',
-    chunk_size=2000,
-    chunk_overlap=0)
+                           chunk_size=max_chunk,
+                           chunk_overlap=0)
 
 code_chunk = bb.split_text(processed_code)
 
-chunk_gt_2000 = [i for i, item in enumerate(code_chunk) if len(item)>2000]
+breach_index = _context_length_check(code_chunk, max_chunk)
 
 code_chunk = [item.replace('**splitpoint**', 'run;') for item in code_chunk]
 
-prompts = [{"code": f'SAS Code Part{i+1}: ```{item}```'} for i,item in enumerate(code_chunk)]
-
+prompts = [{"code": f'SAS Code Part{i + 1}: ```{item}```'} for i, item in enumerate(code_chunk)]
 
 # pyspark_code = await chain.abatch(prompts[0:3], config={"max_concurrency": 3})
 #
@@ -120,37 +137,57 @@ file = open("converted_code.txt", "w+", encoding='utf-8')
 file_sas = open("converted_code_sas.txt", "w+", encoding='utf-8')
 
 print(len(prompts))
-range_loop = (len(prompts)/2)
-if range_loop%2 != 0:
+range_loop = (len(prompts) / 2)
+if range_loop % 2 != 0:
     range_loop = int(range_loop) + 1
 else:
     range_loop = range_loop
 
+
+def chunk_checking_and_hitting(prompt, i, max_chunk):
+    manual_conversion_text = '''\n\n\n########Manual conversion needed since context length is more than what model 
+    allows###########\n {}
+    ##########manual block ends#############\n\n\n'''
+    if (len(prompts[i * 2]) <= max_chunk) & (len((i * 2) + 2) <= max_chunk):
+        pyspark_code = chain.batch(prompt[i * 2: (i * 2) + 2])
+        converted_code = os.linesep.join([str(elem) for elem in pyspark_code])
+    elif len(prompts[i * 2]) > max_chunk:
+        pyspark_code = chain.batch(prompt[(i * 2) + 2])
+        pyspark_code = [manual_conversion_text.format(prompts[i * 2])] + pyspark_code
+        converted_code = os.linesep.join([str(elem) for elem in pyspark_code])
+    elif len(prompts[(i * 2) + 2]) > max_chunk:
+        pyspark_code = chain.batch(prompt[i * 2])
+        pyspark_code = pyspark_code + [manual_conversion_text.format((i * 2) + 2)]
+        converted_code = os.linesep.join([str(elem) for elem in pyspark_code])
+    else:
+        pyspark_code = [manual_conversion_text.format(i * 2)] + [manual_conversion_text.format((i * 2) + 2)]
+        converted_code = os.linesep.join([str(elem) for elem in pyspark_code])
+    return converted_code
+
+
 print(f'total loop {range_loop}')
+
+
 t_0 = time.time()
 for i in range(range_loop):
     print(f'loop number {i}')
     print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
     try:
-        print(i*2, (i*2)+2)
-        pyspark_code = chain.batch(prompts[i*2: (i*2)+2])
+        print(i * 2, (i * 2) + 2)
+        pyspark_code = chain.batch(prompts[i * 2: (i * 2) + 2])
         converted_code = os.linesep.join([str(elem) for elem in pyspark_code])
         with open("converted_code.txt", "a", encoding='utf-8') as f:
             f.write(converted_code)
         with open("converted_code_sas.txt", "a", encoding='utf-8') as f:
             converted_code_sas = os.linesep.join(
-                [str(elem['code']) for elem in prompts[i*2: (i*2)+2]]
+                [str(elem['code']) for elem in prompts[i * 2: (i * 2) + 2]]
             )
             f.write(converted_code_sas)
-
-        # for s in chain.stream(item):
-        # #     print(s.content, end="", flush=True)
-
-    except Exception as e:
-        print(i*2, (i*2)+2)
+    except openai.APITimeoutError as e:
         print(e)
-        time.sleep(60)
-        pyspark_code = chain.batch(prompts[i*2: (i*2)+2])
+        print(i * 2, (i * 2) + 2)
+        chain2 = chat_prompt | model2 | StrOutputParser()
+        pyspark_code = chain2.batch(prompts[i * 2: (i * 2) + 2])
         converted_code = os.linesep.join([str(elem) for elem in pyspark_code])
         with open("converted_code.txt", "a", encoding='utf-8') as f:
             f.write(converted_code)
@@ -160,12 +197,26 @@ for i in range(range_loop):
             )
             f.write(converted_code_sas)
 
+        # for s in chain.stream(item):
+        # #     print(s.content, end="", flush=True)
+
+    except openai.BadRequestError as e:
+        print(i * 2, (i * 2) + 2)
+        print(e)
+        time.sleep(60)
+        converted_code = chunk_checking_and_hitting(prompts, i, max_chunk)
+        with open("converted_code.txt", "a", encoding='utf-8') as f:
+            f.write(converted_code)
+        with open("converted_code_sas.txt", "a", encoding='utf-8') as f:
+            converted_code_sas = os.linesep.join(
+                [str(elem['code']) for elem in prompts[i * 2: (i * 2) + 2]]
+            )
+            f.write(converted_code_sas)
     # if i%2==0:
     t_1 = time.time()
-    if (t_1-t_0) < 60:
-        print(f"put on sleep for matching RPM {print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))}")
-        time.sleep(60-(t_1-t_0))
+    if (t_1 - t_0) < 60:
+        print(f"put on sleep for matching RPM {(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))}")
+        time.sleep(60 - (t_1 - t_0))
         t_0 = t_1
-
 
 # pyspark_code = chain.invoke({"code": code})
